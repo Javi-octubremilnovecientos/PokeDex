eb8c33837573464893e240d8b1d3c22f057bed88
javier lombardero <javier.lombar2@gmail.com>
2024-10-18 12:03:34 +0200

    funcionalidad filtrar por tipo pokemon 2.0

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -1,46 +1,35 @@
 const listaPokemos = document.querySelector(".pokemon-todos");
-const pokeContainer = document.querySelector("main")
+const pokeContainer = document.querySelector("main");
 
+for (i = 0; i <= 155; i++) {
+  const id = i;
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+  async function fetchData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
 
-for(i=0;i <=155; i++){
-
-const id = i
-const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-
-
-async function fetchData() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-   
-    
-    let tipoUno = "";
-    let tipoDos = "" ; 
-   if(data.types.length > 1 ){
-     tipoUno = data.types[0].type.name
-     tipoDos =  data.types[1].type.name
-   }else{
-       
-      tipoUno = data.types[0].type.name
-      tipoDos = false
-    
-   }
-   const {name, height, weight } = data 
-  pintarCard(name,tipoUno, tipoDos,height,weight)
-  
-  } catch (error) {
-  // alert("error")
+      let tipoUno = "";
+      let tipoDos = "";
+      if (data.types.length > 1) {
+        tipoUno = data.types[0].type.name;
+        tipoDos = data.types[1].type.name;
+      } else {
+        tipoUno = data.types[0].type.name;
+        tipoDos = false;
+      }
+      const { name, height, weight } = data;
+      pintarCard(name, tipoUno, tipoDos, height, weight);
+    } catch (error) {
+      // alert("error")
+    }
   }
-}
 
-const pintarCard = (name,tipoUno, tipoDos, height, weight)=>{
-
-  const card = document.createElement("div")
-  if(tipoUno  && tipoDos){
-    card.innerHTML = 
-      `<div id="todos">
+  const pintarCard = (name, tipoUno, tipoDos, height, weight) => {
+    const card = document.createElement("div");
+    if (tipoUno && tipoDos) {
+      card.innerHTML = `<div id="todos">
           <div class="pokemon-todos">
             <div class="pokemon">
               <p class="pokemon-id-back">#${id}</p>
@@ -62,10 +51,9 @@ const pintarCard = (name,tipoUno, tipoDos, height, weight)=>{
                 </div>
               </div>
             </div>
-      </div>`
-  }else{
-    card.innerHTML = 
-    `<div id="todos">
+      </div>`;
+    } else {
+      card.innerHTML = `<div id="todos">
         <div class="pokemon-todos">
           <div class="pokemon">
             <p class="pokemon-id-back">#${id}</p>
@@ -87,61 +75,50 @@ const pintarCard = (name,tipoUno, tipoDos, height, weight)=>{
               </div>
             </div>
           </div>
-    </div>`
-  }
-  const primerTipo = card.querySelector(".pokemon-tipos .electric")
-  const segundoTipo = card.querySelector(".pokemon-tipos .tipo ")
-  primerTipo.setAttribute("id", tipoUno)
-  segundoTipo.setAttribute("id", tipoDos)
-  pokeContainer.append(card)
-
-}
-
-
-
-fetchData();
-
-}
-
-
-
-const botones = document.querySelectorAll("nav ul li button")
-botones.forEach((boton)=>{
-  boton.addEventListener("click",(event)=>{
-    let tipo = event.target.id
-    
- 
-   
-    for(k=0;k <=155; k++){
-
-      const id = k
-      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-      
-    async function fetchData() {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-      
-    const tipoUno = data.types[0].type.name
-    const  tipoDos =  data.types[1].type.name
-   
-  
-     const {name, height, weight } = data 
-   
-     
-       if( tipoUno === tipo){
-        pintarCard(name, height,tipoUno,tipoDos, weight)
-       
-      } else if( tipoDos === tipo){
-        pintarCard(name, height,tipoUno, tipoDos, weight)
-     
-      }
-      pintarCard()
-      } catch (error) {
-       //alert("error")
-      }
+    </div>`;
     }
-      fetchData()
-  }})
-})
+    const primerTipo = card.querySelector(".pokemon-tipos .electric");
+    const segundoTipo = card.querySelector(".pokemon-tipos .tipo ");
+    primerTipo.setAttribute("id", tipoUno);
+    segundoTipo.setAttribute("id", tipoDos);
+    pokeContainer.append(card);
+  };
+  fetchData();
+}
 
+const botones = document.querySelectorAll("nav ul li button");
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", (event) => {
+    let tipo = event.target.id;
+
+    pokeContainer.innerHTML = "";
+
+    for (k = 0; k <= 155; k++) {
+      const id = k;
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+      async function fetchData() {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+
+          const tipoUno = data.types[0].type.name;
+          const tipoDos = data.types[1].type.name;
+
+          const { name, height, weight } = data;
+
+          if (tipoUno === tipo) {
+            pintarCard(name, height, tipoUno, tipoDos, weight);
+          } else if (tipoDos === tipo) {
+            pintarCard(name, height, tipoUno, tipoDos, weight);
+          }
+          pintarCard();
+        } catch (error) {
+          //alert("error")
+        }
+      }
+      fetchData();
+    }
+  });
+});
