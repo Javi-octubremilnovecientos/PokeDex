c16cb867980d9dd7e0b3ea9ee5c438b566cf6143
javier lombardero <javier.lombar2@gmail.com>
2024-10-18 12:01:02 +0200

    funcion filtrar por tipo de pokemon

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -1,24 +1,37 @@
 const listaPokemos = document.querySelector(".pokemon-todos");
 const pokeContainer = document.querySelector("main")
 
-for(i=0;i<=156;i++){
+
+
+for(i=0;i <=155; i++){
+
 const id = i
 const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+
 
 async function fetchData() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    
-    const tipoUno = data.types[0].type.name
-    const tipoDos =  data.types[1].type.name
-    const {name, height, weight } = data 
    
-    pintarCard(name,tipoUno, tipoDos,height,weight)
+    
+    let tipoUno = "";
+    let tipoDos = "" ; 
+   if(data.types.length > 1 ){
+     tipoUno = data.types[0].type.name
+     tipoDos =  data.types[1].type.name
+   }else{
+       
+      tipoUno = data.types[0].type.name
+      tipoDos = false
+    
+   }
+   const {name, height, weight } = data 
+  pintarCard(name,tipoUno, tipoDos,height,weight)
   
   } catch (error) {
-   
-  
+  // alert("error")
   }
 }
 
@@ -66,7 +79,7 @@ const pintarCard = (name,tipoUno, tipoDos, height, weight)=>{
               <h1 class="pokemon-nombre">${name}</h1>
               <div class="pokemon-tipos">
                   <p class="electric">${tipoUno}</p>
-              
+                  <p class="tipo">${tipoDos}</p>
               </div>
               <div class="pokemon-stats">
                 <p>${height}m</p>
@@ -76,10 +89,59 @@ const pintarCard = (name,tipoUno, tipoDos, height, weight)=>{
           </div>
     </div>`
   }
+  const primerTipo = card.querySelector(".pokemon-tipos .electric")
+  const segundoTipo = card.querySelector(".pokemon-tipos .tipo ")
+  primerTipo.setAttribute("id", tipoUno)
+  segundoTipo.setAttribute("id", tipoDos)
   pokeContainer.append(card)
-  }
+
+}
+
 
 
 fetchData();
 
 }
+
+
+
+const botones = document.querySelectorAll("nav ul li button")
+botones.forEach((boton)=>{
+  boton.addEventListener("click",(event)=>{
+    let tipo = event.target.id
+    
+ 
+   
+    for(k=0;k <=155; k++){
+
+      const id = k
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+      
+    const tipoUno = data.types[0].type.name
+    const  tipoDos =  data.types[1].type.name
+   
+  
+     const {name, height, weight } = data 
+   
+     
+       if( tipoUno === tipo){
+        pintarCard(name, height,tipoUno,tipoDos, weight)
+       
+      } else if( tipoDos === tipo){
+        pintarCard(name, height,tipoUno, tipoDos, weight)
+     
+      }
+      pintarCard()
+      } catch (error) {
+       //alert("error")
+      }
+    }
+      fetchData()
+  }})
+})
+
